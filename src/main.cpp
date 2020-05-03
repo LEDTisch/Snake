a39b35dd269498867af72a4abd712f568f1aa6c9
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-05-03 19:34:49 +0200

    test pixel rot

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ LEDTisch ledtisch=LEDTisch(10,15,1);
 
 void setup() {
     ledtisch.init(10);
+    ledtisch.clear();
+    ledtisch.setcolor(255,0,0);
+    ledtisch.drawkoordinatensystem(5,5);
+    ledtisch.show();
 }
 
 void loop() {
