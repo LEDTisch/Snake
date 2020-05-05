1e52346ddb0d1c67bd64e82b02829829d15e36a2
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-05-05 10:10:21 +0200

    länge der snake jetzt unbegrenzt

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -16,7 +16,7 @@ void Snake::init(int _pin){
 
 void Snake::draw(){
     for(int l=0;l<this->length;l++){
-        ledtisch.setcolor(255-l*50,l*50,0);
+        ledtisch.setcolor(255-l*10,l*10,0);
         ledtisch.drawkoordinatensystem(this->snake[l][0],this->snake[l][1]);
     }
 
@@ -47,11 +47,18 @@ void Snake::addPixel(){
 
 void Snake::move(){
 ledtisch.drawkoordinatensystem(snake[length-1][0],snake[length-1][1],0x000000);
+/*
         snake[2][0]=snake[1][0];
                 snake[2][1]=snake[1][1];
 
     snake[1][0]=snake[0][0];
         snake[1][1]=snake[0][1];
+        */
+        for(int i=length-1;i>0;i--){
+            snake[i][0]=snake[i-1][0];
+            snake[i][1]=snake[i-1][1];
+
+        }
 
 switch(direction){
     case 0://rechts
--- src/Snake.h
+++ src/Snake.h
@@ -3,7 +3,7 @@
 
 class Snake{
     private:
-        int length=3;
+        int length=10;
         int arraySizeX=10;
         int arraySizeY=15;
         //int snake[10][15][2];
